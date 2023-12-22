d2829b1f0cdc495e25f0011019cb9b5afb027ab4
kachadarshan <darshan.kacha12@gmail.com>
2023-12-22 09:15:35 +0530

    Done

--- MyApp/src/Screen/HomeScreen.tsx
+++ MyApp/src/Screen/HomeScreen.tsx
@@ -56,6 +56,48 @@ const HomeScreen = () => {
             phoneno: '123456789',
             email: 'yes@test.com',
             logo: 'https://bfsi.eletsonline.com/wp-content/uploads/2023/05/YES-BANK.jpg'
+        },
+        {
+            id: 7,
+            name: 'SBI',
+            phoneno: '123456789',
+            email: 'sbi@test.com',
+            logo: "https://w7.pngwing.com/pngs/190/639/png-transparent-state-bank-of-india-thane-branch-banking-in-india-bank-blue-text-branch-thumbnail.png"
+        },
+        {
+            id: 8,
+            name: 'KOTAK',
+            phoneno: '123456789',
+            email: 'kotak@test.com',
+            logo: 'https://companieslogo.com/img/orig/KOTAKBANK.NS-36440c5e.png?t=1593960269'
+        },
+        {
+            id: 9,
+            name: 'HDFC',
+            phoneno: '123456789',
+            email: 'hdfc@test.com',
+            logo: "https://startinup.up.gov.in/wp-content/uploads/2022/12/hdfc-logo.png"
+        },
+        {
+            id: 10,
+            name: 'ICICI',
+            phoneno: '123456789',
+            email: 'icici@test.com',
+            logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCFV1rGmie-WZdJxKilLAXQHVxD4BkP7bIxgH-nhaVbA&s'
+        },
+        {
+            id: 11,
+            name: 'BOB',
+            phoneno: '123456789',
+            email: 'bob@test.com',
+            logo: "https://1000logos.net/wp-content/uploads/2021/06/Bank-of-Baroda-icon.png"
+        },
+        {
+            id: 12,
+            name: 'YES',
+            phoneno: '123456789',
+            email: 'yes@test.com',
+            logo: 'https://bfsi.eletsonline.com/wp-content/uploads/2023/05/YES-BANK.jpg'
         }
     ]
 
